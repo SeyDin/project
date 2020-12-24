00b296365d9fc9d0cfd8e7ae0988aad822308da5
Saint <jafaralex@mail.ru>
2020-12-24 21:30:14 +0400

    fist commit

---
+++ src/main/java/com/greenbox/test/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.greenbox.test.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("fuck");
+        System.out.println("fuck");
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
