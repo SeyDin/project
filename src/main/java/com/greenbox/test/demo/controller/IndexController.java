a999f6fa74c0e8327b6d797791da76c243c99c02
Saint <jafaralex@mail.ru>
2021-01-01 13:05:16 +0400

    Added: index view. Thymeleaf working now.

---
+++ src/main/java/com/greenbox/test/demo/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.greenbox.test.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(value ="/index")
+    public String index (ModelMap modelMap){
+        //System.out.println("Index controller method");
+        modelMap.addAttribute("message", "WelcomeToHell=)");
+        return "index";
+    }
+}
