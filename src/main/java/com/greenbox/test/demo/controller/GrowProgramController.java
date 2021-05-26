18dad5c768c1157374b615cd3e10f28b9b79ac2a
Saint <jafaralex@mail.ru>
2021-05-26 15:55:46 +0400

    Fixed registration of a new User if he had wrong data.

--- src/main/java/com/greenbox/test/demo/controller/GrowProgramController.java
+++ src/main/java/com/greenbox/test/demo/controller/GrowProgramController.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -119,6 +118,7 @@ public class GrowProgramController {
                 return "error/403";
             }
         }
+
         GrowProgram growProgram = new GrowProgram();
         growProgram.setId(growProgramRegistrationForm.getId());
         growProgram.setName(growProgramRegistrationForm.getName());
